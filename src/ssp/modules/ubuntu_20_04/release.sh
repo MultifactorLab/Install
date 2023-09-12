e38a1878a54ebd667bf23988a5a054ce084dfa77
Aleksey Pashkov <pashkov.a.n@yandex.ru>
2023-09-12 23:20:56 +0600

    WIP: ubuntu 20.04 support was added

---
+++ src/ssp/modules/ubuntu_20_04/release.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+write_log "\nInstalling application components"
+write_log " - Downloading Self Service Portal release..."
+{  
+    sudo wget https://github.com/MultifactorLab/multifactor-selfservice-portal/releases/latest/download/MultiFactor.SelfService.Linux.Portal.zip
+} &>> "${MFA_OUTPUT_FILE}"
+assert_success
+
+write_log " - Extracting files..."
+{  
+    sudo unzip -o MultiFactor.SelfService.Linux.Portal.zip -d "$MFA_APP_DIR"
+    sudo rm MultiFactor.SelfService.Linux.Portal.zip
+} &>> "${MFA_OUTPUT_FILE}"
+assert_success
