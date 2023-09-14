46659c37e10b7e5e0fdf33df03ed03f918c13c39
Aleksey Pashkov <pashkov.a.n@yandex.ru>
2023-09-14 15:16:19 +0600

    unzip was added

--- src/ssp/modules/ubuntu_20.04/release.sh
+++ src/ssp/modules/ubuntu_20.04/release.sh
@@ -7,6 +7,11 @@ write_log " - Downloading Self Service Portal release..."
 } &>> "${MFA_OUTPUT_FILE}"
 assert_success
 
+{
+    sydo apt-get install unzip
+} &>> "${MFA_OUTPUT_FILE}"
+assert_success
+
 write_log " - Extracting files..."
 {  
     sudo unzip -o MultiFactor.SelfService.Linux.Portal.zip -d "$MFA_APP_DIR"
