544dbb7b634e5681eb846e56776ce84beb1905fc
Vladimir Zimin <v.zimin@multifactor.ru>
2024-11-20 11:56:29 +0300

    clean, update readme

--- src/ssp/modules/astra_1.8_x86-64/filesystem.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-write_log "\nSetting up directories"
-{
-    sudo mkdir -p "${MFA_PARENT_DIR}" "${MFA_WORK_DIR}" "${MFA_APP_DIR}" "${MFA_LOG_DIR}" "${MFA_KEY_STOR_DIR}"
-} &>> "${MFA_OUTPUT_FILE}"
-assert_success
